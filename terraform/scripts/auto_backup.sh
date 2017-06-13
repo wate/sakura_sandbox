b564944e48613ac130dfba1a63f095562b2e508d
wate <pc.poisoning@gmail.com>
2017-06-14 01:03:51 +0900

    コメントを調整

--- terraform/scripts/auto_backup.sh
+++ terraform/scripts/auto_backup.sh
@@ -7,7 +7,7 @@ SAKURACLOUD_ACCESS_TOKEN_SECRET=[YOUR_API_SECRET]
 # 世代数
 NUMBRE_OF_GENERATUINS=3
 
-# 曜日＋時間指定での自動バックアップ(タグ名:backup_week_[mon|tue|wed|thu|fri|sat|sun]_「0-23」)
+# 曜日＋時間指定での自動バックアップ(タグ名:backup_week_[mon|tue|wed|thu|fri|sat|sun]_[0-23])
 WEEKLY_TAG=backup_week_`LANG=C date +%a_%k| tr "[:upper:]" "[:lower:]"|tr -d " "`
 echo "Auto backup tag name: ${WEEKLY_TAG}"
 for DISK_ID in `usacloud disk list --quiet --tags ${WEEKLY_TAG}`; do
@@ -19,7 +19,7 @@ for DISK_ID in `usacloud disk list --quiet --tags ${WEEKLY_TAG}`; do
 	# done;
 done;
 
-# 時間指定での自動バックアップ(タグ名:backup_week_[mon|tue|wed|thu|fri|sat|sun]_「0-23」)
+# 時間指定での自動バックアップ(タグ名:backup_hour_[0-23])
 HOURLY_TAG=backup_hour_`date +%k|tr -d " "`
 echo "Auto backup tag name: ${HOURLY_TAG}"
 for DISK_ID in `usacloud disk list --quiet --tags ${HOURLY_TAG}`; do
